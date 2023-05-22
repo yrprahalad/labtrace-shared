62e1db1a6167a29ebb09fc7837f945e6739e0d8f
rakshi <rakshi.yr@gmail.com>
2023-05-22 21:52:59 +0530

    Added Custorm Errors

--- helpers/errors.ts
+++ helpers/errors.ts
@@ -1,5 +1,5 @@
 export const apiErrorMessage = (error: any) => {
-    if (error.response.data) {
+    if (error.response && error.response.data) {
         return error.response.data
     } else if (error.message) {
         return error.message;
