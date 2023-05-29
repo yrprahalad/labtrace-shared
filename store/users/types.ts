0d3f72e59ccd2e073262f5d4902102fe911b209b
rakshi <rakshi.yr@gmail.com>
2023-05-29 22:44:09 +0530

    Added Password Param

--- store/users/types.ts
+++ store/users/types.ts
@@ -8,6 +8,7 @@ export interface User {
     username: string,
     firstname: string,
     lastname: string,
+    password: string,
     email: string,
     employeeNumber: string,
     description: string,
