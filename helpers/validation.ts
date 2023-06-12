11d3543e0b661b2b80bde2689fc853ac8848f12d
rakshi <rakshi.yr@gmail.com>
2023-06-12 22:05:49 +0530

    Added validation file

---
+++ helpers/validation.ts
@@ -0,0 +1,14 @@
+export const isPassword = (password: string): boolean => {
+    const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return strongRegex.test(password);
+};
+
+export const isEmail = (email: string): boolean => {
+    const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return regexEmail.test(email)
+};
+
+export const isUUID = (uuid: string): boolean => {
+    const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidPattern.test(uuid);
+};
